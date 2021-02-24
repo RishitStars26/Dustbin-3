a5491f7e91d270a40eb7a17824e8dd7f85007082
RishitStars26 <73429629+RishitStars26@users.noreply.github.com>
2021-02-24 12:20:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dustbin1,dustbin2,dustbin3;
+var paperBall;
+var ground;
+var engine;
+
+function setup() {
+	createCanvas(1200, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+
+	//var ground  = createSprite(width/2,height-35,width,10)
+	//ground.shapeColor = ("yellow");
+
+	dustbin1 = new Dustbin(700,640,20,60);
+	dustbin2 = new Dustbin(900,640,20,60);
+	dustbin3 = new Dustbin(800,660,180,80); 
+
+	paperBall = new Paper(295,660,25); 
+	ground = new Ground(600,675,1200,20);
+
+	Engine.run(engine);
+}
+function draw() {
+  background("white");
+  rectMode(CENTER);
+  Engine.update(engine)
+
+  paperBall.display();
+  ground.display();
+  //dustbin1.display();
+  //dustbin2.display();
+  dustbin3.display();  
+
+  //drawSprites();
+}
+function keyPressed(){
+	if(keyCode === 32){
+		Matter.Body.applyForce(paperBall.body,paperBall.body.position,{x:85,y:-85})
+	}
+}
+
+
+
